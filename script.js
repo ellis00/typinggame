1525d7274aaf0b7491ae8945140188726ddb596c
ellis00 <elliss00008@gmail.com>
2024-09-21 09:24:38 -0700

    script.js

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ document.getElementById('typingInput').addEventListener('input', function(e) {
 
     switch(input) {
         case 'love':
-            noteElement.innerHTML = "<p><strong>Love:</strong> 22 September 2023. I'm really happy I asked you, despite how cringey and panicked the whole thing was. I love you so much:3 .</p>";
+            noteElement.innerHTML = "<p><strong>:</strong>  I'm really happy I asked you that day, despite how cringey and panicked the whole thing was. I love you so much and promise to make it up and a lot more romantical for you:3 .</p>";
             break;
         case 'sweetheart':
-            noteElement.innerHTML = "<p><strong>Sweetheart:</strong> That's also you! I always love the petnames you give me, so I want to return the favour and give you a bunch. This is by far my favourite though so I'm going to overuse it :p</p>";
+            noteElement.innerHTML = "<p><strong>:</strong> That's also you! I always love the petnames you give me, so I want to return the favour and give you a bunch. This is by far my favourite though so I'm going to overuse it :p</p>";
             break;
         case 'scarlett':
-            noteElement.innerHTML = "<p><strong>Scarlett:</strong> That's you! I'm very glad that it was you I met and asked</p>";
+            noteElement.innerHTML = "<p><strong>:</strong> That's you! I'm very glad that it was you that I met and asked</p>";
+            break;
+        case '22 september':
+            noteElement.innerHTML = "<p><strong>:</strong> 22nd September 2023, the day we got together. Now I get a bunch of gifts around the same time as my birthday is right after :p</p>";
+            break;
+        case 'pink':
+            noteElement.innerHTML = "<p><strong>:</strong> Pink. Both of our favorite colours, hence why every gift you got today has an ongoing colour theme..</p>";
+            break;
+         case 'ellis':
+            noteElement.innerHTML = "<p><strong>:</strong> me? don't put my name, these gifts are meant to be about u .. .·°՞(≧□≦)՞°·.</p>";
             break;
         default:
             noteElement.innerHTML = ""; // Clear the message if no recognized word
